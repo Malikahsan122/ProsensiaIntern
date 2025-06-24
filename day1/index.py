72466ffa6f638171d86764baa3472808acf71869
malikahsan122 <malikahsii345@gmail.com>
2025-06-24 16:57:44 +0500

    Intial Commit

---
+++ day1/index.py
@@ -0,0 +1,11 @@
+
+int_input = int(input("Enter an integer: "))
+float_input = float(input("Enter a float: "))
+string_input = input("Enter a string: ")
+
+
+print("You entered:")
+print("Integer: {int_input}")
+print("Float: {float_input}")
+print("String: {string_input}")
+
